577e1dbb577149d8875cc8c194a60443107e456e
Axron <aarmeji77@gmail.com>
2020-06-24 14:47:52 -0700

    Moving AI to C++

---
+++ Source/TestingGroundsFPS/ChooseNextWaypoint.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+     auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+     auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+     UE_LOG(LogTemp, Warning, TEXT("Waypoit index: %i"), Index);
+     return EBTNodeResult::Succeeded;
+}
